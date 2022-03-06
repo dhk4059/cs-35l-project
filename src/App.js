302b8c9c0035c26b6d59a8cd772b16e29c69fc03
bluepaw59 <96216128+bluepaw59@users.noreply.github.com>
2022-03-06 02:35:39 -0800

    submit button added

--- src/App.js
+++ src/App.js
@@ -7,13 +7,17 @@ import 'bootstrap';
 function App() {
   return (
     <div className="App">
+      {/* Star ratings from star-rating-component */}
       <StarRating />
       <form>
+        {/* text box input form from bootstrap*/}
         <div class="form-group">
           <label for="housingComments">Additional Comments:</label>
-          <textarea class="form-control" id="housingComments" rows="10" placeholder="Any additional comments about the housing?"></textarea>
+          <textarea class="form-control" id="housingComments" rows="7" placeholder="Any additional comments about the housing?"></textarea>
         </div>
       </form>
+      {/* button to submit review */}
+      <button type="button" class="SubmitReview">Submit</button>
     </div>
   );
 }
